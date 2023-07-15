ca191b7ae2083252d4103eb507f3ff8cd5b68854
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-07-15 19:13:01 +0300

    Add BookDto and fix xml return

--- WebApplication1/Utilities/AutoMapper/MappingProfile.cs
+++ WebApplication1/Utilities/AutoMapper/MappingProfile.cs
@@ -9,6 +9,7 @@ namespace WebApplication1.Utilities.AutoMapper
         public MappingProfile()
         {
             CreateMap<BookDtoForUpdate, Book>();
+            CreateMap<Book, BookDto>();
         }
     }
 }

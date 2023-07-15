54a46e07edac5b3d8ff2e058b126b9151ee66702
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-07-15 19:29:15 +0300

    .csv output format implementation

---
+++ WebApplication1/Extentions/IMvcBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication1.Utilities.Formatters;
+
+namespace WebApplication1.Extentions
+{
+    public static class IMvcBuilderExtensions
+    { 
+        public static IMvcBuilder AddCustomCsvFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
+        
+    }
+}

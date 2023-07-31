c22be002d4ce2fee05c6367ba85aef27ba27d5ac
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-07-31 23:50:00 +0300

    PriceOutOfRangeBadRequestException and GetAllBooksAsync return filtres.

---
+++ Entities/Exceptions/PriceOutOfRangeBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Exceptions
+{
+    public class PriceOutOfRangeBadRequestException : BadRequestException
+    {
+        public PriceOutOfRangeBadRequestException() :
+            base("Maximim price should be less than 1000 and greater than 10.")
+        {
+            
+        }
+    }
+}

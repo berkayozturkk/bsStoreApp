c97eca5460542f54108da4af37aba1e76f9b83db
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-08-03 02:21:07 +0300

    Sort method.

--- Repositories/EFCore/Extentions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extentions/BookRepositoryExtensions.cs
@@ -3,8 +3,10 @@ using Entities.RequestFeatures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Repositories.EFCore.Extentions
 {
@@ -26,6 +28,47 @@ namespace Repositories.EFCore.Extentions
                 .Where(b => b.Title
                 .ToLower()
                 .Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books,
+            string orderByQueryString)
+        {
+            if (!string.IsNullOrEmpty(orderByQueryString))
+                return books.OrderBy(b => b.Id);
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+
+            var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public 
+                | BindingFlags.Instance);
+
+            var orderQueryBuilder = new StringBuilder();
+
+
+            // title ascending , price descending, id ascending,
+            foreach (var param in orderParams)
+            {
+                if(string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+
+                var objectProperty = propertyInfos
+                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
+                    StringComparison.InvariantCultureIgnoreCase));
+
+                var direction = param.EndsWith(" desc") ? "descending"
+                                                        : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} " +
+                    $"{direction},");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',',' ');
+
+            if( orderQuery is null)
+                return books.OrderBy(b => b.Id);
+
+            return books.OrderBy(orderQuery);
         }
     }
 }

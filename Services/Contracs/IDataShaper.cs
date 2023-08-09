3d9c34e6e1701e67d87cb19c1a70dcf75e1e36cb
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-08-09 14:39:24 +0300

    .

---
+++ Services/Contracs/IDataShaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracs
+{
+    public interface IDataShaper<T>
+    {
+        IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities ,string filedString);
+        ExpandoObject ShapeData(T entity,string filedString);
+    }
+}

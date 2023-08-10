5989dbce1fc6c927656a812c85e0b2c3b0f70541
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-08-10 18:43:47 +0300

    Added LinkModel

---
+++ Entities/LinkModels/Link.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+    public class Link
+    {
+        public string? HRef { get; set; }
+        public string? Relation { get; set; }
+        public string? Method { get; set; }
+
+        public Link()
+        {
+                
+        }
+
+        public Link(string? hRef, string? relation, string? method)
+        {
+            HRef = hRef;
+            Relation = relation;
+            Method = method;
+        }
+    }
+}
